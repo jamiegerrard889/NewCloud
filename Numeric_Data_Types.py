52dc5370142fb4d1f1d5b25dd4622776693dfb9e
jamiegerrard889 <jamiegerrard889@gmail.com>
2024-05-12 18:50:32 +0000

    Add files via upload

---
+++ Numeric_Data_Types.py
@@ -0,0 +1,44 @@
+print("Python has three numeric types: int, float, and complex")
+myValue=1
+print(1)
+print(type(1))
+print(str(1) + " is of the data type " + str(type(1)))
+myValue=3.14
+print(3.14)
+print(type(3.14))
+print(str(3.14) + " is of the data type " + str(type(3.14)))
+myValue=5j
+print(5j)
+print(type(5j))
+print(str(5j) + " is of the data type " + str(type(5j)))
+myValue=True
+print(True)
+print(type(True))
+print(str(True) + " is of the data type " + str(type(True)))
+myValue=False
+print(False)
+print(type(False))
+print(str(False) + " is of the data type " + str(type(False)))
+
+
+
+
+Python has three numeric types: int, float, and complex
+1
+<class 'int'>
+1 is of the data type <class 'int'>
+3.14
+<class 'float'>
+3.14 is of the data type <class 'float'>
+5j
+<class 'complex'>
+5j is of the data type <class 'complex'>
+True
+<class 'bool'>
+True is of the data type <class 'bool'>
+False
+<class 'bool'>
+False is of the data type <class 'bool'>
+
+
+Process exited with code: 0
